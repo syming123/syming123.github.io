1cd866efe900f8ada104d64002a3a205abeae539
sym <1308940118@qq.com>
2022-06-18 22:03:21 +0800

    1

---
+++ app/hello.js
@@ -0,0 +1,12 @@
+var vm = new Vue({
+    el: '#hello',
+    data: {
+        t1: "hello",
+        t2: "i am sym"
+    },
+    methods: {
+        f1: function() {
+            return  "nice to see u";
+        }
+    }
+})
